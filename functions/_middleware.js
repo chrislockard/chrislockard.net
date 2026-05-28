b5ecf390771395fb37d0c1acc61058d41accfd5c
Chris Lockard <github@chrislockard.net>
2026-05-28 09:13:58 -0400

    Update _middleware.js
    Point to www instead of naked domain

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -3,7 +3,7 @@ export const onRequest = [
   async ({ request, next }) => {
     const url = new URL(request.url);
     if (url.hostname === "chrislockard-net.pages.dev") {
-      url.hostname = "chrislockard.net";
+      url.hostname = "www.chrislockard.net";
       return Response.redirect(url.toString(), 301);
     }
     return next();
