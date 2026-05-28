0aed723e8cb320209964104b7089ceada5f6b2c4
Chris Lockard <github@chrislockard.net>
2026-05-28 09:05:45 -0400

    Chore: Add _middleware.js for CF redirection
    Redirect visits to the naked Pages origin

---
+++ functions/_middleware.js
@@ -0,0 +1,11 @@
+// functions/_middleware.js
+export const onRequest = [
+  async ({ request, next }) => {
+    const url = new URL(request.url);
+    if (url.hostname === "chrislockard-net.pages.dev") {
+      url.hostname = "chrislockard.net";
+      return Response.redirect(url.toString(), 301);
+    }
+    return next();
+  },
+];
